850d21ff67284e482372038173ae19c6b1d32935
Карл <dio.lloyd@gmail.com>
2025-11-11 13:28:30 +0300

    Исправления по замечаниям

--- hw03-spring-boot/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -12,7 +12,6 @@ import ru.otus.hw.exceptions.QuestionReadException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
